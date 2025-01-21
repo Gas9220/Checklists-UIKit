0fbc42b9d112aceabd2787045271a19d17ad0c02
Gaspare Monte <gaspare.monte@rawfish.com>
2025-01-21 16:57:23 +0100

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Gaspare Monte on 21/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
